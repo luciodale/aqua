84edc4e5b2f18f49ecab2299b015e37c1df912ec
Lucio D'Alessandro <lda@juxt.pro>
2019-12-01 03:21:55 +0000

    Handle big scroll jumps

--- src/aqua/utils.cljs
+++ src/aqua/utils.cljs
@@ -27,24 +27,33 @@
   (js/console.log m))
 
 (defn run-effect!
-  [{:keys [container-node animations] :as m}]
-  (doseq [{:keys [from to animate]} animations]
-    (let [d (distance-to-bottom container-node)]
-      (when (or (and (= :initial from) (= :no-stop to))
-                (and (= :initial from) (<= d to))
-                (and (>= d from) (= :no-stop to))
-                (and (>= d from) (<= d to)))
-        (animate d m)))))
+  [{:keys [container-node animations] :as m} s]
+  (let [d (distance-to-bottom container-node)]
+    (doseq [{:keys [from to animate k]} animations]
+      (do
+        (cond
+          (and (< (k @s) to) (> d to))
+          (animate to m)
+          (and (< d from) (> (k @s) from))
+          (animate from m)
+          (or
+           (and (= :initial from) (= :no-stop to))
+           (and (= :initial from) (<= d to))
+           (and (>= d from) (= :no-stop to))
+           (and (>= d from) (<= d to)))
+          (animate d m))
+        (swap! s assoc k d)))))
 
 (defn register-effect
   [m debug?]
-  (when debug? (debug-print m))
-  (when-let [init-f (:initiate m)]
-    (init-f (distance-to-bottom (:container-node m)) m))
-  (run-effect! m)
-  (js/window.addEventListener
-   "optimizedScroll"
-   #(run-effect! m)))
+  (let [s (atom nil)]
+    (when debug? (debug-print m))
+    (when-let [init-f (:initiate m)]
+      (init-f (distance-to-bottom (:container-node m)) m))
+    (run-effect! m s)
+    (js/window.addEventListener
+     "optimizedScroll"
+     #(run-effect! m s))))
 
 (defn collect-from-ids
   [^js obj ids]
@@ -69,7 +78,7 @@
 (defn subscribe-effect
   [{:keys [container-id inline external animations initiate debug?]}]
   (let [container-node {:container-node (js/document.getElementById container-id)}
-        animations {:animations animations}
+        animations {:animations (mapv #(assoc % :k (keyword (gensym))) animations)}
         initiate {:initiate initiate}]
     (if external
       (let [^js object-node (js/document.getElementById (:object-id external))]
