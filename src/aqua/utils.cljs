75b51af344b61199fcd9962c7efb85fee38e4650
Lucio D'Alessandro <lda@juxt.pro>
2019-12-02 22:15:11 +0000

    Add useful debug messages

--- src/aqua/utils.cljs
+++ src/aqua/utils.cljs
@@ -30,14 +30,18 @@
   [{:keys [container-node animations] :as m}]
   (let [d (distance-to-bottom container-node)]
     (doseq [{:keys [from to animate]} animations]
-      (cond
-        (or
-         (and (= :initial from) (= :no-stop to))
-         (and (= :initial from) (<= d to))
-         (and (>= d from) (= :no-stop to))
-         (and (>= d from) (<= d to))) (animate d m)
-        (> d to) (animate to m)
-        (< d from) (animate from m)))))
+      (try
+        (cond
+          (or
+           (and (= :initial from) (= :no-stop to))
+           (and (= :initial from) (<= d to))
+           (and (>= d from) (= :no-stop to))
+           (and (>= d from) (<= d to))) (animate d m)
+          (> d to) (animate to m)
+          (< d from) (animate from m))
+        (catch js/Error e
+          (js/console.err
+           (str "Exception thrown in one of the animation functions" e)))))))
 
 (defn register-effect
   [m debug?]
@@ -51,24 +55,36 @@
      #(run-effect! m))))
 
 (defn collect-from-ids
-  [^js obj ids]
+  [^js obj ids debug?]
   (into {}
         (map (fn [id]
-               {id (.getElementById obj id)})
+               (let [node (.getElementById obj id)]
+                 (when (and (nil? node) debug?)
+                   (js/console.warn
+                    (str "The id " id
+                         " could not be found or its element has not been loaded to the DOM."
+                         " Make sure the element is rendered AND loaded before the subscribe function is invoked.")))
+
+                 {id (.getElementById obj id)}))
              ids)))
 
 (defn collect-from-classes
-  [^js obj classes]
+  [^js obj classes debug?]
   (into {}
         (map (fn [class]
-               {class (array-seq
-                       (.getElementsByClassName obj class))})
+               (let [nodes (array-seq (.getElementsByClassName obj class))]
+                 (when (and (= 0 (count nodes)) debug?)
+                   (js/console.warn
+                    (str "The class " class
+                         " could not be found or its elements have not been loaded to the DOM."
+                         " Make sure the class elements are rendered AND loaded before the subscribe function is invoked.")))
+                 {class nodes}))
              classes)))
 
 (defn collect-nodes
-  [m ^js obj]
-  {:ids (collect-from-ids obj (:ids m))
-   :classes (collect-from-classes obj (:classes m))})
+  [m ^js obj debug?]
+  {:ids (collect-from-ids obj (:ids m) debug?)
+   :classes (collect-from-classes obj (:classes m) debug?)})
 
 (defn subscribe-effect
   [{:keys [container-id inline external animations initiate debug?]}]
@@ -80,9 +96,9 @@
       container-node
       animations
       initiate
-      {:inline (collect-nodes inline js/document)}
+      {:inline (collect-nodes inline js/document debug?)}
       (when-let [^js object-node (js/document.getElementById (:object-id external))]
 
         {:object-node object-node
-         :external (collect-nodes external (.-contentDocument object-node))}))
+         :external (collect-nodes external (.-contentDocument object-node) debug?)}))
      debug?)))
